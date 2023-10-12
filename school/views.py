7e71bd35f7875774d43ae8803031b3e85375b000
Sankaran <vineethvineeth088@gmail.com>
2023-10-12 12:03:05 +0530

    Successfully implemented login and registration databases

--- school/views.py
+++ school/views.py
@@ -1,14 +1,24 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
 
-
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        cpassword = request.POST.get('cpassword')
+        if password != cpassword:
+            error = 'password and confirm password must be matched'
+            return render(request,'register.html',{'error':error})
+        else:
+            user = User.objects.create(username=username,email=username,password=cpassword)
+            msg = 'Account created successfully'
+            return render(request,'login.html',{'msg':msg})
+    return render(request,'login.html')
 
 
 def register(request):
@@ -16,7 +26,19 @@ def register(request):
 
 
 def profile(request):
-    return render(request, 'profile.html', {'profile': 'enabled'})
+    if request.method == 'POST':
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        if username == '' or password == '':
+            error = 'User name or password must not be empty'
+            return render(request,'login.html',{'error':error})
+        else:
+            user = User.objects.filter(username=username,password=password)
+            if user is not None:
+                return render(request, 'profile.html', {'profile': 'enabled'})
+            else:
+                error = 'Invalid username or password'
+                return render(request, 'login.html', {'error': error})
 
 
 def order(request):
